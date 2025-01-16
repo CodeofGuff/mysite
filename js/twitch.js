ca4b5556435f958cfb8f29d8eeb25a7026e41021
David <161796631+CodeofGuff@users.noreply.github.com>
2025-01-15 19:18:31 -0800

    Update twitch.js

--- js/twitch.js
+++ js/twitch.js
@@ -2,6 +2,6 @@
 new Twitch.Embed("twitch-embed", {
     width: 800,
     height: 500,
-    channel: "GUFFRE", // Replace with your Twitch channel name
-    parent: ["guff.netlify.app"] // Replace with your domain, e.g., "example.com"
+    channel: "GUFFRE", 
+    parent: ["guff.netlify.app"]
 });
